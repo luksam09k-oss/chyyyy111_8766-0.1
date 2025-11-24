2ae5f0b29161eba907d7193d342a6d2fa056d117
luksam09k-oss <luksam09k@gmail.com>
2025-11-24 12:36:07 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,5 +1,4 @@
 const socket = io();
-
 const username = localStorage.getItem("username");
 
 socket.emit("join-room", { room: "chat", username }, (res) => {
@@ -9,25 +8,33 @@ socket.emit("join-room", { room: "chat", username }, (res) => {
 
 socket.on("new-message", addMessage);
 
+// Render lateral de usuarios
 socket.on("user-list", (users) => {
   const side = document.getElementById("users");
   side.innerHTML = "";
   users.forEach(u => {
     const div = document.createElement("div");
-    div.innerHTML = `<img src="/avatar/${u.avatarId || 'default.png'}" width="30"> ${u.username}`;
+    div.style.cursor = "pointer";
+    div.style.display = "flex";
+    div.style.alignItems = "center";
+    div.style.marginBottom = "5px";
+    div.innerHTML = `<img src="/avatar/${u.avatarId || 'default.png'}" width="30" style="border-radius:50%;margin-right:5px;"> <b>${u.username}</b>`;
     div.onclick = () => alert(`Perfil de ${u.username}`);
     side.appendChild(div);
   });
 });
 
+// Mensajes
 function addMessage(m) {
   const box = document.getElementById("messages");
   const line = document.createElement("div");
+  line.style.marginBottom = "5px";
   line.innerHTML = `<b>${m.user}:</b> ${m.text}`;
   box.appendChild(line);
   box.scrollTop = box.scrollHeight;
 }
 
+// Enter para enviar
 document.getElementById("msgBox").addEventListener("keypress", e => {
   if (e.key === "Enter") sendMsg();
 });
