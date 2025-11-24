8518f453743e692c957ef30dba2fd4ef393c8f42
luksam09k-oss <luksam09k@gmail.com>
2025-11-24 01:48:34 -0400

    Create Message.js

---
+++ Message.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  user: String,
+  text: String,
+  time: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
