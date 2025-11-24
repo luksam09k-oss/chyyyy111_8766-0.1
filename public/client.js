84ff62661d2fc8e7d382bda5f630394b02194671
luksam09k-oss <luksam09k@gmail.com>
2025-11-24 12:42:44 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,12 +1,15 @@
 const socket = io();
 const username = localStorage.getItem("username");
+const rol = localStorage.getItem("rol");
 
-socket.emit("join-room", { room: "chat", username }, (res) => {
+socket.emit("join-room", { room: "chat", username, rol }, (res) => {
   if (!res.ok) return alert("No puedes entrar");
   res.history.forEach(addMessage);
 });
 
 socket.on("new-message", addMessage);
+socket.on("system-message", addMessage);
+socket.on("clear-chat", () => document.getElementById("messages").innerHTML = "");
 
 // Render lateral de usuarios
 socket.on("user-list", (users) => {
@@ -14,25 +17,15 @@ socket.on("user-list", (users) => {
   side.innerHTML = "";
   users.forEach(u => {
     const div = document.createElement("div");
-    div.style.cursor = "pointer";
     div.style.display = "flex";
     div.style.alignItems = "center";
     div.style.marginBottom = "5px";
+    div.style.cursor = "pointer";
     div.innerHTML = `<img src="/avatar/${u.avatarId || 'default.png'}" width="30" style="border-radius:50%;margin-right:5px;"> <b>${u.username}</b>`;
     div.onclick = () => alert(`Perfil de ${u.username}`);
     side.appendChild(div);
   });
 });
-
-// Mensajes
-function addMessage(m) {
-  const box = document.getElementById("messages");
-  const line = document.createElement("div");
-  line.style.marginBottom = "5px";
-  line.innerHTML = `<b>${m.user}:</b> ${m.text}`;
-  box.appendChild(line);
-  box.scrollTop = box.scrollHeight;
-}
 
 // Enter para enviar
 document.getElementById("msgBox").addEventListener("keypress", e => {
@@ -46,4 +39,13 @@ function sendMsg() {
     if (!res.ok) alert("Error enviando mensaje");
   });
   document.getElementById("msgBox").value = "";
+}
+
+function addMessage(m) {
+  const box = document.getElementById("messages");
+  const line = document.createElement("div");
+  line.style.marginBottom = "5px";
+  line.innerHTML = `<b>${m.user || 'SYSTEM'}:</b> ${m.text}`;
+  box.appendChild(line);
+  box.scrollTop = box.scrollHeight;
 }
