8f9cc9cb3dd9bc4bef4fb0cdaa3ded718f87be9e
luksam09k-oss <luksam09k@gmail.com>
2026-01-02 23:52:56 -0400

    Update User.js

--- User.js
+++ User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String },               // si tienes login local
+  password: { type: String },               
   rol: { type: String, default: "user" },
   avatarId: { type: String, default: "default.png" },
   banned: { type: Boolean, default: false }
